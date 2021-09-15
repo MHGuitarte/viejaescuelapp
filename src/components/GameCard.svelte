c28ee6512ba7dad4854aac79008fea61215866bb
mhurtado <manuel.hurtado@eydevelopment.com>
2021-09-15 09:03:58 +0200

    develop default app entry point

---
+++ src/components/GameCard.svelte
@@ -0,0 +1,30 @@
+<script>
+	import { goto } from '$app/navigation';
+
+	export let title;
+	export let bgImage;
+	export let href;
+
+	const handleClick = () => {
+		goto(href, {
+			state: {
+				title
+			}
+		});
+	};
+</script>
+
+<ion-card on:click={handleClick} class="card-container" style="--background: url({bgImage})" />
+
+<style lang="scss">
+	.card-container {
+		aspect-ratio: 21 / 9;
+		display: flex;
+		flex-flow: column-reverse;
+		margin: 40px 20px;
+		background-size: 100% 100%;
+		background-repeat: no-repeat;
+        background-color: white;
+	}
+
+</style>
