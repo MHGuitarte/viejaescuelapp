3f3529dd9489e74af40e6bb6d00c09d4d9e3c029
mhurtado <manuel.hurtado@eydevelopment.com>
2021-09-22 09:16:41 +0200

    feat: create simple divider component

---
+++ src/components/Divider.svelte
@@ -0,0 +1,20 @@
+<script>
+	export let width = 1;
+	export let color = 'black';
+</script>
+
+<div
+	class="divider"
+	style={`
+    height: ${width}px;
+    background-color: ${color};
+`}
+/>
+
+<style scoped lang="scss">
+	.divider {
+		content: '';
+		display: block;
+		widows: 100%;
+	}
+</style>
